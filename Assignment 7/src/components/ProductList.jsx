4b529b07399d25854404812c57375b3fbbbad4fc
vishangl <161571588+vishangl@users.noreply.github.com>
2025-02-11 19:04:36 +0530

    2203031080035 Assignment-37

---
+++ Assignment 7/src/components/ProductList.jsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+
+function ProductList() {
+  const products = [
+    { id: 1, name: "Phone", price: "$699" },
+    { id: 2, name: "Laptop", price: "$1200" },
+    { id: 3, name: "Headphones", price: "$199" },
+  ];
+
+  return (
+    <div>
+      <h2>Product List</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductList;
