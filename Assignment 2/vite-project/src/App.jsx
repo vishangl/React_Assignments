2cbbdda704b298665f5403d51ef073db36617768
vishangl <161571588+vishangl@users.noreply.github.com>
2025-02-11 11:39:22 +0530

    assignment2 - 2203031080035

---
+++ Assignment 2/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Greetings from "./components/Greet"
+
+const App = () => {
+  return (
+    <>
+      <Greetings/>
+      <Greetings/>
+      <h2>Again the same</h2>
+      <Greetings/>
+      <Greetings/>
+    </>
+  )
+}
+
+export default App;
---
+++ Assignment 2/vite-project/src/components/Greet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Greet() {
+  return (
+    <div>
+      <p>This sounds good</p>
+    </div>
+  )
+}
+
+export default Greet
